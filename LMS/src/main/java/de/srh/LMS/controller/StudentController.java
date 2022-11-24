12ea8c06b7ecf75ef3b2615ab6e7a93a5767ccb0
Lakusan <Andreas.Lakus@googlemail.com>
2022-11-24 23:06:21 +0100

    EMail can occur only once -> email exists http 500 "email taken". POST Request user registration implemented.

--- LMS/src/main/java/de/srh/LMS/controller/StudentController.java
+++ LMS/src/main/java/de/srh/LMS/controller/StudentController.java
@@ -3,9 +3,7 @@ package de.srh.LMS.controller;
 import de.srh.LMS.entity.Student;
 import de.srh.LMS.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,10 +16,12 @@ public class StudentController {
     public StudentController(StudentService studentService){
         this.studentService = studentService;
     }
-
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
     }
-
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
 }
--- LMS/src/main/java/de/srh/LMS/service/StudentService.java
+++ LMS/src/main/java/de/srh/LMS/service/StudentService.java
@@ -1,13 +1,12 @@
 package de.srh.LMS.service;
 
-import de.srh.LMS.controller.StudentController;
 import de.srh.LMS.entity.Student;
 import de.srh.LMS.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -20,6 +19,15 @@ public class StudentService {
 
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
 
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository
+                .findStudentByEMail(student.getEmail());
+        // TODO: Validation - email valid??
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
     }
 }
